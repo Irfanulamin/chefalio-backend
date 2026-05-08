d0b474dafde6f80e17686de79838579dcfaaa6c2
Irfanulamin <irfanulamin10@gmail.com>
2026-05-08 06:07:18 +0600

    refactor: clean up commented code and streamline bootstrap process in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,53 +7,14 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import express, { Request, Response } from 'express';
 
-const server = express();
+// const server = express();
 
-const createNestServer = async (expressInstance: express.Express) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-    { rawBody: true },
-  );
-
-  app.use(cookieParser());
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      forbidUnknownValues: true,
-    }),
-  );
-
-  app.enableCors(
-    process.env.NODE_ENV === 'production'
-      ? {
-          origin: process.env.ALLOWED_ORIGIN,
-          credentials: true,
-        }
-      : {
-          origin: 'http://localhost:3000',
-          credentials: true,
-        },
-  );
-
-  await app.init();
-};
-
-let isServerReady = false;
-
-export default async function handler(req: Request, res: Response) {
-  if (!isServerReady) {
-    await createNestServer(server);
-    isServerReady = true;
-  }
-  server(req, res);
-}
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, { rawBody: true });
+// const createNestServer = async (expressInstance: express.Express) => {
+//   const app = await NestFactory.create(
+//     AppModule,
+//     new ExpressAdapter(expressInstance),
+//     { rawBody: true },
+//   );
 
 //   app.use(cookieParser());
 
@@ -66,11 +27,50 @@ export default async function handler(req: Request, res: Response) {
 //     }),
 //   );
 
-//   app.enableCors({
-//     origin: 'http://localhost:3000',
-//     credentials: true,
-//   });
+//   app.enableCors(
+//     process.env.NODE_ENV === 'production'
+//       ? {
+//           origin: process.env.ALLOWED_ORIGIN,
+//           credentials: true,
+//         }
+//       : {
+//           origin: 'http://localhost:3000',
+//           credentials: true,
+//         },
+//   );
 
-//   await app.listen(5000);
+//   await app.init();
+// };
+
+// let isServerReady = false;
+
+// export default async function handler(req: Request, res: Response) {
+//   if (!isServerReady) {
+//     await createNestServer(server);
+//     isServerReady = true;
+//   }
+//   server(req, res);
 // }
-// bootstrap();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { rawBody: true });
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  await app.listen(5000);
+}
+bootstrap();
